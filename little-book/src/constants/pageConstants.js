032ae616e0845f8ad2f7e13cd23000372890ce5a
Ranjith Saravanan <125631729+ranjithks-cdw@users.noreply.github.com>
2023-08-09 15:29:00 +0530

    init commit

---
+++ little-book/src/constants/pageConstants.js
@@ -0,0 +1,4 @@
+export const FALLBACK_IMAGE = {
+    BLOG: 'https://himalyantrips.com/wp-content/uploads/2019/03/travel-and-tourism.jpg',
+    USER: 'https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg'
+};
